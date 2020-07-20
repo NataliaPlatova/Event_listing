14d001a0ac0aa3d3e593c47b30c4d4b7d5e81e61
Natalia <n.platova@hotmail.com>
2020-07-20 18:42:36 +0700

    Updates

---
+++ precache-manifest.dc1741b76d181a9341225e4301e9a99b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "836d2dbfc50a69433198bd62d4baa738",
+    "url": "/event_listing/index.html"
+  },
+  {
+    "revision": "686c0b1a8ccbda03b20b",
+    "url": "/event_listing/static/css/main.1c07a7df.chunk.css"
+  },
+  {
+    "revision": "b53744634f45f6d10fcd",
+    "url": "/event_listing/static/js/2.c0452434.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/event_listing/static/js/2.c0452434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686c0b1a8ccbda03b20b",
+    "url": "/event_listing/static/js/main.b3a2c4ba.chunk.js"
+  },
+  {
+    "revision": "a8ed3d3b5eeb25afcc28",
+    "url": "/event_listing/static/js/runtime-main.1bb1cb24.js"
+  }
+]);
